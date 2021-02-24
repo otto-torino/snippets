eb616c7c5b79c5d7c2d9a3f6f5b8a790fc33ed7c
Daniele Pira <daniele.pira@otto.to.it>
2021-02-24 10:53:23 +0100

    fix python boolean expressions

--- python-boolean-expressions.py
+++ python-boolean-expressions.py
@@ -1,9 +1,9 @@
 # A ⟷ B
 
 def iif(a, b):
-    return ~(bool(a) ^ bool(b))
+    return not (bool(a) ^ bool(b))
 
 # A ⟶ B
 
 def imply(a, b):
-    return not bool(a) or bool(b)
+    return (not bool(a)) or bool(b)
